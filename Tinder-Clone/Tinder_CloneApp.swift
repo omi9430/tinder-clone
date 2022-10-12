2878e9d5c33f23fddebdf1a85c7845d5d9c7c17c
Umair <u.ishrat@drovox.com>
2022-10-12 19:31:00 +0300

    Initial Commit

---
+++ Tinder-Clone/Tinder_CloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  Tinder_CloneApp.swift
+//  Tinder-Clone
+//
+//  Created by MacBooK Pro on 12/10/2022.
+//
+
+import SwiftUI
+
+@main
+struct Tinder_CloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
